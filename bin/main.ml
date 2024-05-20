4e9548ad320cd3a494b7727dddc6652a261582d7
immotare <immortal3409fgh@gmail.com>
2024-05-20 23:14:39 +0900

    複数のastNodeをprintするよう変更

--- bin/main.ml
+++ bin/main.ml
@@ -2,9 +2,16 @@ let () =
   Printexc.print(fun () ->
     try
       let lexbuf = Lexing.from_channel stdin in
+      let node_cnt = ref 0 in
       while true do
         let result = Parser.main Lexer.token lexbuf in
-        print_string (AstNode.PPrint.to_string result); print_newline()
+        print_string ("<" ^ (string_of_int !node_cnt) ^ ">");
+        print_newline();
+        print_string (AstNode.PPrint.to_string result);
+        print_newline();
+        print_string "-------------------------";
+        print_newline();
+        node_cnt := !node_cnt + 1;
       done
     with
       Lexer.EOF -> exit 0
